9698a7e5386a4262db722626a6cf0c238cd262ef
Karthik Y S <karthikys@Karthiks-MacBook-Air.local>
2017-11-05 23:14:38 +0530

    Added Test to config

--- Test_Status_Check.py
+++ Test_Status_Check.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-import Status_Check
+from src import Status_Check
 
 def test_response_200():
     assert Status_Check.get_status('http://httpstat.us/200') == 200
@@ -24,4 +24,4 @@ def test_response_524():
     assert Status_Check.get_status('http://httpstat.us/524') == 524
 
 def test_url_retrieve():
-    assert Status_Check.read_urls(os.getcwd() + '/urls_test.txt') == ['http://httpstat.us/200','http://httpstat.us/201','http://httpstat.us/305','http://httpstat.us/403','http://httpstat.us/404','http://httpstat.us/500','http://httpstat.us/524']
+    assert Status_Check.read_urls(os.getcwd()/src + '/urls_test.txt') == ['http://httpstat.us/200','http://httpstat.us/201','http://httpstat.us/305','http://httpstat.us/403','http://httpstat.us/404','http://httpstat.us/500','http://httpstat.us/524']
